e908ed789a320d2c45bf6ee31464c3285ae73365
Alberto Aragon <alberto@alturasoluciones.com>
2018-08-25 10:21:26 -0500

    Basic layout

--- src/AppDragDropDemo.js
+++ src/AppDragDropDemo.js
@@ -1,10 +1,68 @@
 import React, { Component } from 'react';
+import './App.css'; 
 
 export default class AppDragDropDemo extends Component {
+  state = {
+    tasks: [{
+      name: "Learn Angular",
+      category: "wip",
+      bgcolor: "yellow"
+    },
+
+    {
+      name: "React",
+      category: "wip",
+      bgcolor: "pink"
+    },
+
+    {
+      name: "Vue",
+      category: "complete",
+      bgcolor: "skyblue"
+    }
+    ]
+  }
+
   render() {
+    let tasks = {
+      wip: [],
+      complete: []
+    }
+
+    this.state.tasks.forEach ((t) => {               
+    tasks[t.category].push(<div 
+        key={t.name}                     
+        onDragStart={(e)=>this.onDragStart(e, t.name)}                    
+        draggable                    
+        className="draggable"                    
+        style={{backgroundColor: t.bgcolor}}>                       
+          {t.name}                
+      </div>);        
+    });
+
     return (
       <div className="container-drag">
-        
+        <h2 className="header">
+          DRAG & DROP DEMO
+        </h2>
+        <div
+          className="wip" 
+          onDragOver={(e) => this.onDragOver(e)}
+        >
+          <span className="task-header">
+            WIP
+          </span>
+          {tasks.wip}
+        </div>
+        <div 
+          className="droppable"
+          onDragOver={(e) => this.onDragOver(e)}
+        >
+          <span className="task-header">
+            COMPLETED
+          </span>
+          {tasks.complete}
+        </div>
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import AppDragDropDemo from './AppDragDropDemo';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+ReactDOM.render(<AppDragDropDemo />, document.getElementById('root'));
